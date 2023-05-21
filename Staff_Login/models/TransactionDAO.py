c5a51fb2ba9b64eddecb8a2bc4c421d0b90923f9
Pranav152 <saipranav152@gmail.com>
2023-05-21 00:39:23 -0400

    Added transaction function after issue_book

--- Staff_Login/models/TransactionDAO.py
+++ Staff_Login/models/TransactionDAO.py
@@ -11,11 +11,12 @@ class TransactionDAO():
 		return result,h
 	
 	def getBook(self,student_id,book_name):
+		print("SELECT book_id FROM transactions WHERE studentUsername= '"+str(student_id)+"' AND bookName= '"+str(book_name)+"' ")
 		result,h = self.db.query_data("SELECT book_id FROM transactions WHERE studentUsername= '"+str(student_id)+"' AND bookName= '"+str(book_name)+"' ")
 		return result,h
 	
 	def update_transaction(self,student_id,book_id):
-		self.db.query("UPDATE transactions SET Done = 1  WHERE book_id = " +str(book_id)+" AND studentUsername= "+str(student_id)+" ")
+		self.db.query("UPDATE transactions SET Done = 1  WHERE book_id = " +str(book_id)+" AND studentUsername= '"+str(student_id)+"' ")
 		self.db.commit()
 	
 	def get_return_date(self,student_id,book_id):
@@ -25,4 +26,10 @@ class TransactionDAO():
 	def update_fine(self,student_id,amount_to_be_added_to_fine):
 		self.db.query("UPDATE transactions SET fine=fine+ "+str(amount_to_be_added_to_fine)+" studentUsername= '"+str(student_id)+"'  ")   
 		self.db.commit()      
-		
+	
+	def issue_book(self,student_id,staffUsername,bookName,book_id):
+		query = "INSERT INTO transactions (studentUsername, staffUsername, bookName, book_id) VALUES ('{}', '{}', '{}', {})"
+		formatted_query = query.format(student_id, staffUsername, bookName,book_id)
+		print(formatted_query)
+		self.db.query(formatted_query)   
+		self.db.commit()     
